6a8900e382f9e102761c5ac6859a448ac732a57d
Steven <sarmiento.steven@gmail.com>
2025-09-04 20:49:30 -0700

    Update build scripts and type for reference address

--- packages/solana-pay/src/create-spl-transfer.ts
+++ packages/solana-pay/src/create-spl-transfer.ts
@@ -110,7 +110,7 @@ export async function createSplTransfer(
   if (reference) {
     const refs = Array.isArray(reference) ? reference : [reference];
     const referenceAccounts: AccountMeta[] = refs.map(ref => ({
-      address: ref as Address,
+      address: ref as Address<string>,
       role: AccountRole.READONLY,
     }));
     
