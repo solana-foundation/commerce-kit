68127e7c2be758a59cd2e05419e657453b429c18
Steven <sarmiento.steven@gmail.com>
2025-09-10 13:51:07 -0700

    Create bundle.d.ts

---
+++ packages/react-sdk/src/iframe-app/bundle.d.ts
@@ -0,0 +1,5 @@
+// This file is auto-generated. Do not edit manually.
+// TypeScript declarations for iframe bundle
+
+export declare const IFRAME_BUNDLE: string;
+export declare const IFRAME_STYLES: string;
